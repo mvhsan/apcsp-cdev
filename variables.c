21f07eb131034354febf6816042562b326a6b2ad
Mason Holst <holstm5051@my.sduhsd.net>
2021-01-06 17:02:14 +0000

    added stuff to variables.c for assignment

--- variables.c
+++ variables.c
@@ -9,4 +9,10 @@ int main()
   a = 2;
   c = a + b;
   printf("Sum of %d and %d is %d\n", a, b, c);
+  printf("a is currently %d and c is currently %d\n", a, c);
+
+  int placeholder = a;
+  a = c;
+  c = placeholder;
+  printf("a is now %d and c is now %d\n", a, c);
 }
